f44e05e0da28f9af7bade79c64785c94357b7626
Sudo Infinity <aminchaudhry.dev@gmail.com>
2023-09-13 18:26:54 -0700

    3: Added map functionality to see trip locations

---
+++ client/src/API.js
@@ -0,0 +1,34 @@
+const API_URL = 'http://localhost:1337'
+
+export async function listTrips() {
+    const response = await fetch(`${API_URL}/api/trips`)
+    return response.json();
+}
+
+export async function createTrip(entry) {
+    const apiKey = entry.apiKey;
+    delete entry.apiKey;
+    const response = await fetch(`${API_URL}/api/logs`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        'X-API-KEY': apiKey, 
+      },
+      body: JSON.stringify(entry),
+    });
+    let json;
+    if (response.headers.get('content-type').includes('text/html')) {
+      const message = await response.text();
+      json = {
+        message,
+      };
+    } else {
+      json = await response.json();
+    }
+    if (response.ok) {
+      return json;
+    }
+    const error = new Error(json.message);
+    error.response = json;
+    throw error;
+  }
